8e0b3385952ccecae4adf6ee90a03623d649fdbf
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2020-12-19 05:12:58 -0800

    clean up

--- tmp_job_run_NoSZ_rlz_70-79.py
+++
@@ -1,5 +0,0 @@
-import numpy as np, prjlib, tools_qrec
-kwargs_ov = {'overwrite':True,'verbose':True}
-kwargs_cmb = {'snmin':70,'snmax':79,'dtype':'dr3_nosz','wtype':'LmaskDR3','ascale':1.,'lmax':2048,'fltr':'cinv','tausig':False}
-kwargs_qrec = {'rlmin':100,'rlmax':2048,'nside':1024,'n0min':1,'n0max':50,'rdmin':1,'rdmax':100,'qlist':['TT'],'rd4sim':True}
-tools_qrec.interface(qrun=['rdn0'],run=['tBH'],kwargs_ov=kwargs_ov,kwargs_cmb=kwargs_cmb,kwargs_qrec=kwargs_qrec)
