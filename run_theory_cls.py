efe523858f079d3b8af35f082b4f6aadb366cfda
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2021-06-22 03:00:31 -0700

    auto update

--- run_theory_cls.py
+++ run_theory_cls.py
@@ -1,128 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import cosmology as cosmo, basic, tqdm, numpy as np
-from scipy.integrate import quad, dblquad
+import basic, numpy as np, camb, reionization as reion
 from scipy.interpolate import InterpolatedUnivariateSpline as spline
 
-
-def xHlogxH(xe):
-    if 1.-xe<1e-6:
-        return 0.
-    else:
-        return (1.-xe)*np.log(1.-xe)
-
-
-def Pr(R,R0,sigma=np.log(2.)):
-    return 1./(R*np.sqrt(2*np.pi*sigma**2)) * np.exp(-(np.log(R/R0))**2/(2.*sigma**2))
-
-
-def Wth(x):
-    return 3./x**3 * (np.sin(x)-x*np.cos(x))
-
-
-def Fk(k,R0):
-    I1 = lambda R: Pr(R,R0)*(4*np.pi*R**3/3.)**2*Wth(k*R)**2
-    I2 = lambda R: Pr(R,R0)*4*np.pi*R**3/3.
-    neum = quad(I1,0.,2e2)[0]
-    deno = quad(I2,0.,2e2)[0]
-    return neum/deno
-
-
-def Gk_muint(k,K,Pk):
-    I = lambda mu: Pk( np.sqrt(k**2-2*k*K*mu+K**2) )
-    return quad(I,-1.,1.)[0]
-
-
-def Gk(k,R0,Pk):
-    vFk = np.vectorize(Fk)
-    vGkint = np.vectorize(Gk_muint)
-    Ki = np.logspace(-3,1,11)
-    dK = Ki[1:]-Ki[:-1]
-    I = np.sum(dK*Ki[:-1]**2*vGkint(k,Ki[:-1],Pk)/(2*np.pi)**2*vFk(Ki[:-1],R0))
-    return I
-
-
-def Ik(k,R0):
-    I1 = lambda R: Pr(R,R0) * R**3 * Wth(k*R)
-    I2 = lambda R: Pr(R,R0) * R**3
-    neum = quad(I1,0.,2e2)[0]
-    deno = quad(I2,0.,2e2)[0]
-    return neum/deno
-
-
-def xe_sym(z,zre=8.,Dz=4.):
-    y = np.power(1.+z,1.5)
-    yre = np.power(1.+zre,1.5)
-    Dy  = 1.5*np.sqrt(1.+zre)*Dz
-    return .5*(1.-np.tanh((y-yre)/Dy))
-
-
-def xe_asym(z,alpha=6.,zend=6.):
-    if z < zend:
-        return 1.
-    if z >= zend and z<20.:
-        return np.power((20.-z)/(20.-zend),alpha)
-    if z >= 20:
-        return 1e-30
-
-
+# define parameters
 cps = {'H0':67.5,'Om':.31,'Ov':.69,'w0':-1.,'wa':0.}
-Obh2 = .0223
+Ob = .0455
 bias = 6.
 R0 = 5.
-model = 'TH'
+#model = 'TH'
+model = 'AS'
 alpha = 0.
+ln = 100
 
-
-k, pk0 = np.loadtxt('../data/other/Pklin.dat',unpack=True)
-k *= cps['H0']/100.
-pk0 /= (cps['H0']/100.)**3
-Pk = spline(k,pk0)
-
-
-zi = np.linspace(1e-4,100.,2000)
-Hzi = basic.cosmofuncs.hubble(zi,**cps)
-rzi = basic.cosmofuncs.dist_comoving(zi,**cps)
-Dzi = basic.cosmofuncs.growth_factor(zi,normed=True,**cps)
-Hz = spline( zi, Hzi )
-rz = spline( zi, rzi )
-Dz = spline( zi, Dzi )
-
-
-# define functions
-
-fac = 7.477e-30*3e24*Obh2 # Eq.(3.43) of Dodelson's Modern Cosmology
-
-Kz  = lambda z: fac**2*(1+z)**4/rz(z)**2/Hz(z)
-if model=='AS': xe = lambda z: xe_asym(z)
-if model=='TH': xe = lambda z: xe_sym(z)
+# define reionization history
+if model=='AS': xe = lambda z: reion.xe_asym(z)
+if model=='TH': xe = lambda z: reion.xe_sym(z)
 
 # tau
-I = lambda z: (1+z)**2/Hz(z) * xe(z)
-print('tau', quad(I,0.,100.)[0] * fac)
+reion.optical_depth(xe,Ob=Ob,**cps)
 
-l  = np.linspace(1,3000,100)
-cl = np.zeros((2,np.size(l)))
+# tau spectrum
+l, cl = reion.compute_cltt(xe,R0=R0,alpha=alpha,bias=bias,lmin=1,lmax=3000,ln=ln,zn=500,Ob=Ob,**cps)
 
-Rz = lambda z: R0*np.power(10.,alpha*(xe(z)-.5))
-if alpha==0.: 
-    Rz = lambda z: R0
+# save
+np.savetxt('../data/tau_'+model+'_R'+str(R0)+'_a'+str(alpha)+'.dat',np.array((l,cl)).T)
 
 '''
-for i, L in enumerate(tqdm.tqdm(l)):
-    k  = lambda z: L/rz(z)
-    Pm = lambda z: Dz(z)**2*Pk(k(z))
-    P1 = lambda z: xe(z) * (1-xe(z)) * ( Fk(k(z),Rz(z)) + Gk(k(z),Rz(z),Pm) )
-    P2 = lambda z: ( xHlogxH(xe(z))*bias*Ik(k(z),Rz(z)) - xe(z) )**2 * Pm(z)
-    I0 = lambda z: Kz(z)*(P1(z)+P2(z))
-    I1 = lambda z: Kz(z)*P1(z)
-    cl[0,i] = quad(I0,0.,100.)[0]
-    cl[1,i] = quad(I1,0.,100.)[0]
-
-np.savetxt('../data/plk/input/forecast_tt_'+model+'_R'+str(R0)+'_a'+str(alpha)+'.dat',np.concatenate((l[None,:],cl)).T)
-'''    
-
 # each z contributions
 zbin = np.arange(5.,20.,1.)
 clz = np.zeros((np.size(l)))
@@ -141,4 +46,4 @@ for zi, zb in enumerate(zbin):
         clz[i] = I0(zb)
 
     np.savetxt('../data/other/tt_'+model+'_R'+str(R0)+'_a'+str(alpha)+'_z'+str(zb)+'.dat',np.array((l,clz)).T)
-
+'''
